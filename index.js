52577c43ea2af3ce818882b72fbee8b7cf18f59d
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-09-26 17:49:15 +0500

    .

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://quickstart-ai.me"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
